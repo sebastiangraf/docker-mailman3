47e32fceb03c7352c3f2fc57e8cf5254ce46beeb
Val <arkhipov.valentin@gmail.com>
2015-07-14 23:37:44 +0100

    Remove test scripts

--- test.sh
+++
@@ -1,8 +0,0 @@
-export DOCKERMAIL_OPENDKIM_PORT_8891_TCP_ADDR=172.17.0.10
-# re=^.*PORT_8891_TCP_ADDR=(.*)$
-# echo "$( printenv | sed 's||1|' )"
-#
-if [[ $(printenv) =~ ^.*PORT_8891_TCP_ADDR=([0-9\.]*) ]] ; then
-  echo "${BASH_REMATCH[1]}"
-  echo "====================="
-fi
